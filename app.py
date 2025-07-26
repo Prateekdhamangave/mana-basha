9effb2c4cdc6a7732e26626eb0d16ba34c9515a7
Prateekdhamangave <dhamangaveamar@gmail.com>
2025-07-26 09:38:01 +0530

    Initial commit: Basic Streamlit setup

---
+++ app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.set_page_config(page_title="Mana Basha", layout="centered")
+
+st.title("🌾 Mana Basha - Telugu Corpus Collector")
+st.markdown("Welcome to the prototype! More features coming soon.")
